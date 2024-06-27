92d64c21d03bb68b747212660848cf9c58016e74
Prateek <prateekj1211@gmail.com>
2024-06-27 22:32:48 +0530

    Added css, font and image modules

--- webpack.common.js
+++ webpack.common.js
@@ -17,4 +17,20 @@ module.exports = {
             inject: 'body'
         }),
     ],
+    module:{
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
 }
